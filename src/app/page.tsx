fa5a4e9eb14b75fd740dea097e43cc17e5cd2776
kumail imran <kumail2306@gmail.com>
2023-08-17 15:31:04 +0100

    added the counter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,22 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import kv from "@vercel/kv";
 
-export default function Home() {
-  return <main>0</main>;
+export default async function Home() {
+  const res = await fetch("https://api.github.com/repos/KYT01/view-counter", {
+    next: { revalidate: 5 },
+  });
+  const data = await res.json();
+
+  const pageViews = await kv.incr("views");
+
+  const myStyle = {
+    p: {
+      fontSize: pageViews + "rem",
+    },
+  };
+  return (
+    <main>
+      <p style={myStyle.p}>{pageViews}</p>
+      <span>⭐{data.stargazers_count}</span>
+    </main>
+  );
 }
